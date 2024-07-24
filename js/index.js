8b7decf3214f317184c4fc49aa35c5f93edefdfa
Daniel Garcêz <Daniel_gzribeiro@outlook.com>
2024-07-23 22:21:09 -0300

    Protifolio-V4.6
    Adicionei icones das habilidades

---
+++ js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// var image1 = document.querySelector("#HTML-LOGO")
+
+// image1.addEventListener("mouseover", function(){
+//     image1 = "../components/icons/HtmlLogoDark.png"
+// })
