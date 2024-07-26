f9ae9cca46a5f953a41e35c66bb938c53d202779
Daniel Garcêz <Daniel_gzribeiro@outlook.com>
2024-07-26 00:17:16 -0300

    Protifolio-V4.6.1
    Testes com Javascript

--- js/index.js
+++ js/index.js
@@ -1,7 +1,36 @@
 "use strict";
 
-// var image1 = document.querySelector("#HTML-LOGO")
+let iconsLight = new Array [
+    URL("../components/icons/HtmlLogo.png"),
+    URL("../components/icons/CssLogo.png"),
+    URL("../components/icons/JsLogo.png"),
+    URL("../components/icons/GithubLogo.png"),
+    URL("../components/icons/NodeLogo.png"),
+    URL("../components/icons/FigmaLogo.png")
+]
 
-// image1.addEventListener("mouseover", function(){
-//     image1 = "../components/icons/HtmlLogoDark.png"
-// })
+let iconsDark = new Array [
+    URL("../components/icons/HtmlLogoDark.png"),
+    URL("../components/icons/CssLogoDark.png"),
+    URL("../components/icons/JsLogoDark.png"),
+    URL("../components/icons/GithubLogoDark.png"),
+    URL("../components/icons/NodeLogoDark.png"),
+    URL("../components/icons/FigmaLogoDark.png")
+]
+
+/*
+Fazer uma lista [] com as URLs das imagens 
+
+*/
+
+function trocarImagem(){
+    let image = document.getElementById("HTML-LOGO");
+    if(image.src = "components/icons/HtmlLogo.png"){
+        image.src = "../components/icons/HtmlLogoDark.png"
+        console.log("mudou para dark")
+    }
+    else if(image.src = "../components/icons/HtmlLogoDark.png"){
+        image.src = "components/icons/HtmlLogo.png"
+        console.log("mudou prara light")
+    }
+}
